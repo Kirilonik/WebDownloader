e4fb30b07accc1f1f70b8c0bad0c80eb215520eb
Kirilonik <gamecoolplay@gmail.com>
2020-04-18 13:05:19 +0400

    I wrote a parser of photos from the VK community. It works through VK API.

---
+++ Vk_parser.py
@@ -0,0 +1,44 @@
+import requests
+import os
+
+
+def take_1000_photos():
+    token = "ee0cbef5ee0cbef5ee0cbef533ee7d9361eee0cee0cbef5b096227ad29d52f6cf1beed1"
+    version = 5.92
+    domian = "ohthumbelina"
+    offset = 0  # Смещение от 0 поста
+    count = 50  # Количество отсмотренных постов
+    all_posts = []
+    while offset < 100:
+        response = requests.get("https://api.vk.com/method/wall.get",
+                                params={"access_token": token, "v": version, "domain": domian, "count": count})
+
+        data = response.json()['response']['items']
+        offset += 100
+        all_posts.extend(data)
+    return all_posts
+
+
+def file_writer(data):
+    num = 1
+    os.chdir("D://")
+    os.makedirs("Nice_Girl", exist_ok=True)
+    for posts in data:
+        try:
+            for i in range(len(posts['attachments'])):
+                with open(os.path.join("Nice_Girl", str(num) + ".png"), "wb") as file_photo:
+                    resp = posts['attachments'][i]['photo']['sizes'][-1]['url']
+                    resp_url = requests.get(resp)
+                    resp_url.raise_for_status()
+                    for chunk in resp_url.iter_content(100000):
+                        file_photo.write(chunk)
+                    print("Фото сохранено", num)
+                    num += 1
+        except KeyError:
+            pass
+
+
+all_post = take_1000_photos()
+file_writer(all_post)
+
+print(1)
